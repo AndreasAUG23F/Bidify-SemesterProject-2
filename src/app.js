a9652941a5496a4c34dc96a4dbccab3cf8d479e9
AndreasAUG23F <andabr00198@stud.noroff.no>
2024-11-18 21:47:55 +0100

    changed the folder structure

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import './styles/styles.css';
+
+import router from './js/router';
+
+await router(window.location.pathname);
--- src/ui/main.js
+++
@@ -1,3 +0,0 @@
-import '../styles/tailwind.css';
-
-console.log('Main.js is loaded and working!');
