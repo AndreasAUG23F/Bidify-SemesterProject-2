b1728f449f82cf1553876c31bd176a2dbe6ee7b5
AndreasAUG23F <andabr00198@stud.noroff.no>
2024-12-03 15:48:06 +0100

    included new footer tags in html files, targeting js file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import './css/styles.css';
 import router from './js/router';
 import { makeHeader } from './js/ui/auth/header';
+import { makeFooter } from './js/ui/auth/footer';
 
 makeHeader();
-
+makeFooter();
 await router(window.location.pathname);
--- src/js/ui/listing/makeListing.js
+++ src/js/ui/listing/makeListing.js
@@ -6,7 +6,6 @@ export const displayListings = (listings) => {
     return;
   }
 
-  // Tailwind grid-styling for container
   listingContainer.className =
     'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6 max-w-6xl mx-auto';
 
