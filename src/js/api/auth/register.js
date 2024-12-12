a6f335ed3875d95f869f3a89f3f2ee98633efefa
AndreasAUG23F <andabr00198@stud.noroff.no>
2024-12-12 15:42:30 +0100

    added jsDoc to register

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,6 +1,7 @@
 /**
  * Handles user registration by sending user details to the registration API.
  * Redirects the user to the login page upon successful registration.
+ * Displays an alert if the registration request fails.
  * @module Register
  * @async
  * @function register
@@ -10,36 +11,6 @@
  * @param {string} params.password - The user's password.
  * @returns {Promise<void>} - No return value.
  * @throws {Error} - Logs and alerts errors if the registration request fails.
- */
-
-/* import { hideLoader, showLoader } from '../../ui/global/loader';
-import { API_AUTH_REGISTER } from '../constants';
-import { headers } from '../header';
-
-export async function register({ name, email, password }) {
-  const body = {
-    name: name,
-    email: email,
-    password: password,
-  };
-
-  showLoader();
-  try {
-    const response = await fetch(API_AUTH_REGISTER, {
-      method: 'POST',
-      headers: headers(),
-      body: JSON.stringify(body),
-    });
-    if (response.ok) {
-      alert('User registered');
-      hideLoader();
-      window.location.href = '/auth/login/';
-    }
-  } catch (error) {
-    alert('An error occurred');
-    console.error(error);
-  }
-}
  */
 
 import { hideLoader, showLoader } from '../../ui/global/loader';
